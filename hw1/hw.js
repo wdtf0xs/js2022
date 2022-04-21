d6d82e76da786195a27eb272e3f0f9e693a0f06e
wdtf0xs <marina94p@gmail.com>
2022-04-21 17:08:44 +0300

    Initial commit

---
+++ hw1/hw.js
@@ -0,0 +1,86 @@
+
+    // console.log(`hello`);
+    // alert(`hello`);
+    // document.write(`hello`);
+    //
+    // console.log(`owu`);
+    // alert(`owu`);
+    // document.write(`owu`);
+    //
+    // console.log(`com`);
+    // alert(`com`);
+    // document.write(`com`);
+    //
+    // console.log(`ua`);
+    // alert(`ua`);
+    // document.write(`ua`);
+    //
+    // console.log(1);
+    // alert(1);
+    // document.write(1);
+
+    let hello;
+    console.log(`hello`);
+    alert(`hello`);
+    document.write(`hello`);
+
+    let owu;
+    console.log(`owu`);
+    alert(`owu`);
+    document.write(`owu`);
+
+    let com;
+    console.log(`com`);
+    alert(`com`);
+    document.write(`com`);
+
+    let ua;
+    console.log(`ua`);
+    alert(`ua`);
+    document.write(`ua`);
+
+    console.log(1);
+    alert(1);
+    document.write(1);
+
+    console.log(10);
+    alert(10);
+    document.write(10);
+
+
+    console.log(-999);
+    alert(-999);
+    document.write(-999);
+
+    console.log(123);
+    alert(123);
+    document.write(123);
+
+    console.log(3.14);
+    alert(3.14);
+    document.write(3.14);
+
+    console.log(2.7);
+    alert(2.7);
+    document.write(2.7);
+
+    console.log(16);
+    alert(16);
+    document.write(16);
+
+    console.log(true);
+    alert(true);
+    document.write(true);
+
+    console.log(false);
+    alert(false);
+    document.write(false)
+
+    let book = {name: 'title', genre: 'criminal', 'pages': 285};
+    console.log(book);
+
+    let book2 = {name: 'blabla', 'pages': 144, genre: 'fantasy', authors: ['aut1', 'aut2']};
+    console.log(book2);
+
+    let firstname = 'Maryna'
+
