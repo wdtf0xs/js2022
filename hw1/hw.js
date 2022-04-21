60b6a60681fa6b739bfde12ee685b12ef4a71603
wdtf0xs <marina94p@gmail.com>
2022-04-21 19:04:19 +0300

    hw1-js

--- hw1/hw.js
+++ hw1/hw.js
@@ -1,16 +1,19 @@
-
+    // let hello;
     // console.log(`hello`);
     // alert(`hello`);
     // document.write(`hello`);
     //
+    // let owu;
     // console.log(`owu`);
     // alert(`owu`);
     // document.write(`owu`);
     //
+    // let com;
     // console.log(`com`);
     // alert(`com`);
     // document.write(`com`);
     //
+    // let ua;
     // console.log(`ua`);
     // alert(`ua`);
     // document.write(`ua`);
@@ -18,63 +21,39 @@
     // console.log(1);
     // alert(1);
     // document.write(1);
-
-    let hello;
-    console.log(`hello`);
-    alert(`hello`);
-    document.write(`hello`);
-
-    let owu;
-    console.log(`owu`);
-    alert(`owu`);
-    document.write(`owu`);
-
-    let com;
-    console.log(`com`);
-    alert(`com`);
-    document.write(`com`);
-
-    let ua;
-    console.log(`ua`);
-    alert(`ua`);
-    document.write(`ua`);
-
-    console.log(1);
-    alert(1);
-    document.write(1);
-
-    console.log(10);
-    alert(10);
-    document.write(10);
-
-
-    console.log(-999);
-    alert(-999);
-    document.write(-999);
-
-    console.log(123);
-    alert(123);
-    document.write(123);
-
-    console.log(3.14);
-    alert(3.14);
-    document.write(3.14);
-
-    console.log(2.7);
-    alert(2.7);
-    document.write(2.7);
-
-    console.log(16);
-    alert(16);
-    document.write(16);
-
-    console.log(true);
-    alert(true);
-    document.write(true);
-
-    console.log(false);
-    alert(false);
-    document.write(false)
+    //
+    // console.log(10);
+    // alert(10);
+    // document.write(10);
+    //
+    //
+    // console.log(-999);
+    // alert(-999);
+    // document.write(-999);
+    //
+    // console.log(123);
+    // alert(123);
+    // document.write(123);
+    //
+    // console.log(3.14);
+    // alert(3.14);
+    // document.write(3.14);
+    //
+    // console.log(2.7);
+    // alert(2.7);
+    // document.write(2.7);
+    //
+    // console.log(16);
+    // alert(16);
+    // document.write(16);
+    //
+    // console.log(true);
+    // alert(true);
+    // document.write(true);
+    //
+    // console.log(false);
+    // alert(false);
+    // document.write(false)
 
     let book = {name: 'title', genre: 'criminal', 'pages': 285};
     console.log(book);
@@ -82,5 +61,35 @@
     let book2 = {name: 'blabla', 'pages': 144, genre: 'fantasy', authors: ['aut1', 'aut2']};
     console.log(book2);
 
-    let firstname = 'Maryna'
+    let firstname = 'Maryna';
+    let middlename = 'Anatoliivna';
+    let lastname = 'Pugachova';
+    // let person = firstname + ' ' + middlename + ' ' + lastname
+    let person = `${firstname} ${middlename} ${lastname}`
+    console.log(person);
 
+
+    // let fn = prompt(`Maryna`);
+    // let mn = prompt(`Anatoliivna`);
+    // let ln = prompt(`Pugachova`);
+    // let age = prompt(28);
+    // let person2 = `${fn} ${mn} ${ln} ${age}`;
+    // console.log(person2);
+
+    let a = 100;
+    let b = '100';
+    let c = true;
+    console.log(typeof a);
+    console.log(typeof b);
+    console.log(typeof c);
+
+    let books = [
+        {name: 'blabla', genre: 'horror', authors: ['auth1', 'auth2'] },
+        {name: 'blabla', genre: 'fantasy', authors: ['dlh', 'shj'] },
+        {name: 'blabla', genre: 'drama', authors: ['qwt', 'euuer'] },
+        {name: 'blabla', genre: 'history', authors: ['xhxdh', 'jrtfr'] }
+    ];
+    console.log(books[0]);
+    console.log(books[1]);
+    console.log(books[2]);
+    console.log(books[3]);
