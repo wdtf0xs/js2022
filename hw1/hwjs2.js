6ef1d82634e4211306206c68dbb83396fbe85365
wdtf0xs <marina94p@gmail.com>
2022-04-27 14:09:50 +0300

    hw2 done

---
+++ hw1/hwjs2.js
@@ -0,0 +1,16 @@
+let a = 100;
+let b = 500;
+let c = "hello";
+let d = "okten";
+const x = 'constant value';
+console.log(a);
+
+{
+    e = 'new value';
+    let f = 'new value';
+}
+
+
+let result = 100 + e;
+
+console.log(result);
---
+++ hw2/jshw.js
@@ -0,0 +1,101 @@
+// - Є змінна х, якій ви надаєте довільне числове значення.
+//     Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
+
+    // let a = 1;
+// if (a >= 0) {
+//     document.write(true)
+// } else {
+//     document.write(false)
+// }
+//
+// let b = 0;
+// if (b >= 0) {
+//     document.write(true)
+// } else {
+//     document.write(false)
+// }
+//
+// let c = -3;
+// if(c>=0) {
+//     document.write(true)
+// } else {
+//     document.write(false)
+//
+// }
+
+
+// - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число (в першу, другу, третю чи четверту частину години).
+
+    // let time = prompt('what quarter is 19 in?');
+    // if (time === 'second quarter') {
+    //     console.log('yes, you are right!');
+    // }
+    // else if (time === 'first quarter') {
+    //     console.log('you wrong!');
+    // }
+    // else if (time === 'third quarter') {
+    //     console.log('you wrong!');
+    // }
+    // else if (time === 'fourth quarter') {
+    //     console.log('you wrong!');
+    // }
+
+
+// - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
+
+    // let day = +prompt('what day is it?');
+    // if (day >=1 && day <= 10) {
+    //     console.log('first decade');
+    // }
+    // else if (day >=11 && day <=20) {
+    //     console.log('second decade');
+    // }
+    // else if (day >=21 && day <=31) {
+    //     console.log('third decade');
+    // }
+
+
+
+// - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа
+// що заплановано на цей день (можна замість плану на день, назву дня англійською).
+
+    //     switch (+prompt('enter the day number')) {
+//     case 1:
+//         console.log('monday');
+//         break;
+//     case 2:
+//         console.log('tuesday');
+//         break;
+//     case 3:
+//         console.log('wednesday');
+//         break;
+//     case 4:
+//         console.log('thirsday');
+//         break;
+//     case 5:
+//         console.log('friday');
+//         break;
+//     case 6:
+//         console.log('saturday');
+//         break;
+//     case 7:
+//         console.log('sunday');
+//         break;
+// }
+
+
+// - Користувач вводить або має два числа.
+//     Потрібно знайти та вивести максимальне число з тих двох .
+//     Також потрібно врахувати коли введені рівні числа.
+
+    // let a = 58;
+    // let b = 74;
+    // if(a>b) {
+    //     console.log(a);
+    // }
+    // else if(b>a)
+    //      {console.log(b);}
+    // else if(a===b){
+    //     console.log('equal');
+    // }
+
